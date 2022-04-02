7f3ed29f837a631fd659ab2c6c700eb1f028346d
Piotr Ludynia <pietrekpl18@gmail.com>
2022-04-02 21:55:53 +0200

    initial commit

---
+++ src/test/java/pietrekpl/userRegistrationAndLogin/UserRegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package pietrekpl.userRegistrationAndLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

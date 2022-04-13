6194bc9f87f16741ff9d5a0841771d28382cbce5
Piotr Ludynia <pietrekpl18@gmail.com>
2022-04-13 09:41:31 +0200

    add method to save user in db

--- src/main/java/pietrekpl/userRegistrationAndLogin/controller/UserRegistrationController.java
+++ src/main/java/pietrekpl/userRegistrationAndLogin/controller/UserRegistrationController.java
@@ -2,7 +2,9 @@ package pietrekpl.userRegistrationAndLogin.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pietrekpl.userRegistrationAndLogin.dto.UserRegistrationDto;
 import pietrekpl.userRegistrationAndLogin.service.UserService;
 
 @Controller
@@ -14,5 +16,11 @@ public class UserRegistrationController {
     public UserRegistrationController(UserService userService) {
         this.userService = userService;
     }
+
+    public String registerUser(@ModelAttribute("user")UserRegistrationDto registrationDto){
+        userService.save(registrationDto);
+        return "redirect:registration?success";
+    }
     
+
 }

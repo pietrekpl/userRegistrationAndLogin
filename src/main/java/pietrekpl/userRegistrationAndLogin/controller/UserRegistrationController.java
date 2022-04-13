0efe7e42ab73a03355628037a4bffc0ee55b971a
Piotr Ludynia <pietrekpl18@gmail.com>
2022-04-13 09:46:36 +0200

    create form to register user

--- src/main/java/pietrekpl/userRegistrationAndLogin/controller/UserRegistrationController.java
+++ src/main/java/pietrekpl/userRegistrationAndLogin/controller/UserRegistrationController.java
@@ -2,7 +2,9 @@ package pietrekpl.userRegistrationAndLogin.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pietrekpl.userRegistrationAndLogin.dto.UserRegistrationDto;
 import pietrekpl.userRegistrationAndLogin.service.UserService;
@@ -17,10 +19,16 @@ public class UserRegistrationController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public String registrationForm(){
+        return "registration";
+    }
+
+    @PostMapping
     public String registerUser(@ModelAttribute("user")UserRegistrationDto registrationDto){
         userService.save(registrationDto);
         return "redirect:registration?success";
     }
-    
+
 
 }
